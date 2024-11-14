dfe7f01b53a6409f5c8cec4f0bc4f26f5a8c7c42
Marissha <balabobalaba71@gmail.com>
2024-11-14 12:11:59 +0300

    Add files via upload

---
+++ HW9.4.py
@@ -0,0 +1,32 @@
+import io
+import random
+from pprint import pprint
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+print(list(map(lambda x, y: x == y, first, second)))
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for data in data_set:
+                file.write(f'{data}\n')
+
+    return write_everything
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+class MysticBall:
+    words = []
+    def __init__(self, *words):
+        for i in words:
+            self.words.append(i)
+
+    def __call__(self):
+        return random.choice(self.words)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
